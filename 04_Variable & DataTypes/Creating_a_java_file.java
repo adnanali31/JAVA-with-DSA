98392f00adf389067e9937565c42e8b6a4644149
Adnan Ali <126818381+adnanali31@users.noreply.github.com>
2023-03-03 17:44:20 +0530

    Create Creating_a_java_file.java
    Created a new Java file "MyJavaProgram.java" that prints "Hello, World!" to the console. This file was added to the repository and committed with the message "Created a new Java file 'MyJavaProgram.java' that contains the code to print 'Hello, World!' to the console." The file can be compiled and executed using a Java compiler and the Java Virtual Machine (JVM).

---
+++ 04_Variable & DataTypes/Creating_a_java_file.java
@@ -0,0 +1,14 @@
+// To create a Java file, follow these steps:
+
+// Open a text editor such as Notepad, Sublime Text, or Eclipse.
+// Create a new file and save it with a ".java" extension. For example, "MyJavaProgram.java".
+// In the text editor, type the Java code for your program. For example, the following code prints "Hello, World!" to the console:
+
+public class MyJavaProgram {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+  }
+}
+
+// Save the Java file.
+// That's it! You have now created a Java file. Note that in order to run the Java program, you will need to compile it first. You can use a Java compiler such as the javac command-line tool to compile your Java code into bytecode, which can then be executed using the Java Virtual Machine (JVM).
