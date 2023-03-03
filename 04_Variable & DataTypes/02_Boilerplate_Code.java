2979f19d03b03868b6c3019d3daf977005e588dc
Adnan Ali <126818381+adnanali31@users.noreply.github.com>
2023-03-03 17:57:39 +0530

    Create 02_Boilerplate_Code.java

---
+++ 04_Variable & DataTypes/02_Boilerplate_Code.java
@@ -0,0 +1,17 @@
+// Boilerplate code refers to the standard code that is required in order to set up a basic Java program. 
+// Here is an example of boilerplate code for a simple Java program:
+
+public class Main {
+    public static void main(String[] args) {
+        // Program logic goes here
+    }
+}
+
+
+// This code sets up a class called "Main" with a main method that serves as the entry point for the program. Any program logic can be added inside the main method.
+
+// The "public" keyword before the class declaration indicates that the class can be accessed from anywhere, while the "static" keyword before the main method declaration means that the method can be called without creating an instance of the class.
+
+// The "String[] args" parameter in the main method allows for command line arguments to be passed into the program.
+
+// This is just an example of boilerplate code and can be modified or expanded upon depending on the needs of the specific program.
